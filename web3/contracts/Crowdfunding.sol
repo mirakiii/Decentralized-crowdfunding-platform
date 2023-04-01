538b1ccad30d71a5857833ab3a4e169b474651df
Arohi23 <arohirathore786@gmail.com>
2023-04-02 01:10:06 +0530

    completed solidity smart contract

--- web3/contracts/Crowdfunding.sol
+++ web3/contracts/Crowdfunding.sol
@@ -17,12 +17,56 @@ contract Crowdfunding {
     mapping(uint256 => Campaign) public campaigns;
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign() {}
+    function createCampaign(address _owner, string memory _title, string memory _description,
+     uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
 
-    function donateToCampaign() {}
+        // is everything ook?
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in future.");
 
-    function getDonators() {}
+        // if ok
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
 
-    function getCampaigns() {}
-    
-}
+        numberOfCampaigns++;
+
+        return numberOfCampaigns-1;
+     }
+
+    function donateToCampaign(uint256 _id) public payable{
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+        
+        if(sent){
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
+
+}  
